25b7abbd326c863f77de49dca4c8f08063564077
Drey <oluwadamilareadeniye@yahoo.com>
2025-08-13 23:33:34 +0100

    static and media settings for production

--- mit811_frontend_project/settings/prod.py
+++ mit811_frontend_project/settings/prod.py
@@ -1,3 +1,17 @@
 from .base import *
 
 DEBUG = False
+ALLOWED_HOSTS = ["bookhive.us", "www.bookhive.us"]
+
+# this is the path to the static folder where css, js and images are stored
+STATIC_DIR = BASE_DIR / '/home/speebndt/bookhive_frontend/static/'
+# path to the media folder 
+MEDIA_ROOT = BASE_DIR / '/home/speebndt/bookhive.us/media/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = '/home/speebndt/bookhive.us/static/'
+MEDIA_URL = 'https://bookhive.us/media/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
